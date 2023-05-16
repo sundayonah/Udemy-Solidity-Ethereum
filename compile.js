21a9b8f571b8381c5b148d96f32aa344275839cd
sundayonah <sundayonah94@gmail.com>
2023-05-16 09:10:39 +0100

    add a working compile

--- compile.js
+++ compile.js
@@ -1,11 +1,41 @@
 // compile code will go here
 
+// const path = require("path");
+// const fs = require("fs");
+// const solc = require("solc");
+
+// const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+// const source = fs.readFileSync(inboxPath, "utf8");
+
+// console.log(solc.compile(source, 1));
+
 const path = require("path");
 const fs = require("fs");
-const solc = require("solc")
+const solc = require("solc");
 
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source, 
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
 
-console.log(solc.compile(source, 1));
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contractBytecode =
+  output.contracts["Inbox.sol"]["Inbox"].evm.bytecode.object;
+const contractAbi = output.contracts["Inbox.sol"]["Inbox"].abi;
+
+console.log("Contract bytecode:", contractBytecode);
+console.log("Contract ABI:", contractAbi);
