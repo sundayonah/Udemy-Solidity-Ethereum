d0250edcfccb002e08c0fe130eb34f882f5f4961
sundayonah <sundayonah94@gmail.com>
2023-05-16 11:58:35 +0100

    testing with mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,28 @@
 // contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+
+const web3 = new Web3(ganache.provider());
+
+// class Car {
+//   park() {
+//     return "Stopped";
+//   }
+
+//   drive() {
+//     return "Vroom";
+//   }
+// }
+
+// describe("Car", () => {
+//   it("Can Park", () => {
+//     const car = new Car();
+//     assert.equal(car.park(), "Stopped");
+//   });
+
+//   it("Can drive", () => {
+//     const car = new Car();
+//     assert.equal(car.drive(), "Vroom");
+//   });
+// });
