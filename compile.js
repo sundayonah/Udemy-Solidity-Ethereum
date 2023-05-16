523e7ac8ddd61d5786e495542845b6e590d53f9c
sundayonah <sundayonah94@gmail.com>
2023-05-16 09:56:01 +0100

    update

--- compile.js
+++ compile.js
@@ -2,10 +2,10 @@
 
 // const path = require("path");
 // const fs = require("fs");
-// const solc = require("solc");
+// const solc = require("solc")
 
-// const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-// const source = fs.readFileSync(inboxPath, "utf8");
+// const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// const source = fs.readFileSync(inboxPath, 'utf8');
 
 // console.log(solc.compile(source, 1));
 
@@ -13,35 +13,31 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const compileContract = () => {
-  const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-  const source = fs.readFileSync(inboxPath, "utf8");
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 
-  const input = {
-    language: "Solidity",
-    sources: {
-      "Inbox.sol": {
-        content: source,
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
       },
     },
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["*"],
-        },
-      },
-    },
-  };
-
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
-  const contractBytecode =
-    output.contracts["Inbox.sol"]["Inbox"].evm.bytecode.object;
-  const contractAbi = output.contracts["Inbox.sol"]["Inbox"].abi;
-
-  return {
-    bytecode: contractBytecode,
-    abi: contractAbi,
-  };
+  },
 };
+//const output = JSON.parse(solc.compileStandard(JSON.stringify(input)));
+//const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-module.exports = compileContract;
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contractBytecode =
+  output.contracts["Inbox.sol"]["Inbox"].evm.bytecode.object;
+const contractAbi = output.contracts["Inbox.sol"]["Inbox"].abi;
+
+console.log("Contract bytecode:", contractBytecode);
+console.log("Contract ABI:", contractAbi);
